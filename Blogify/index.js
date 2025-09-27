57e18f0666829670c334a291c6e1c289069b44f1
Rohit-Sharma3843 <157728855+Rohit-Sharma3843@users.noreply.github.com>
2025-09-27 16:47:30 +0530

    Update index.js

--- Blogify/index.js
+++ Blogify/index.js
@@ -13,7 +13,7 @@ const uri = process.env.MONGO_URI;
 app.use(express.static(path.resolve("./Public")));
 app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
-app.set("views", path.resolve("./Views"));
+app.set("views", path.join(__dirname, "Views"));
 app.use(cookieParser());
 app.use(authCheck("token"));
 connect(uri);
@@ -28,4 +28,5 @@ app.listen(PORT, () => {
   console.log("Server is live.");
 });
 module.exports = app;
+
 
