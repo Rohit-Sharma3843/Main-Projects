e990881594c16a4bd820ccc94cb5c34d514e4686
Rohit-Sharma3843 <157728855+Rohit-Sharma3843@users.noreply.github.com>
2025-09-29 23:33:54 +0530

    Update connection.js

--- Blogify/connection.js
+++ Blogify/connection.js
@@ -1,5 +1,23 @@
 const mongoose = require("mongoose");
-function connect(url) {
-  return mongoose.connect(url);
+
+let cached = global.mongo; 
+
+if (!cached) {
+  cached = global.mongo = { conn: null, promise: null };
 }
+
+async function connect(uri) {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }).then((mongoose) => mongoose);
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
 module.exports = connect;
