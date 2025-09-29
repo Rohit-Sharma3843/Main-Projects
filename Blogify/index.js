d7231305648eb804f19973c1bc874e5cd3bf0984
Rohit-Sharma3843 <157728855+Rohit-Sharma3843@users.noreply.github.com>
2025-09-29 22:34:01 +0530

    Update index.js

--- Blogify/index.js
+++ Blogify/index.js
@@ -10,7 +10,7 @@ const authCheck = require("./Middlewares/authentication");
 const blogRouter = require("./Routes/blog");
 const blog = require("./Models/blog");
 const commentRouter = require("./Routes/comment");
-app.use(express.static(path.resolve("./Public")));
+app.use(express.static(path.join(__dirname, "Public")));
 app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "Views"));
@@ -28,5 +28,6 @@ app.listen(PORT, () => {
   console.log("Server is live.");
 });
 module.exports = app;
+
 
 
